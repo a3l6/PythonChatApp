c58cc5135273073f8b4d6f1fab3d04b0d4410b06
Emilio Mendoza <707011@smcdsb.on.ca>
2022-11-02 10:39:16 -0400

    Basic server func

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+SERVER = "10.110.41.216"
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello!")
