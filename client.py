51070867fac857539a4ba46ef18d2e8626044bde
EmilioMendoza <Ironislife39@gmail.com>
2022-11-02 19:30:29 -0400

    Added Nicknames

--- client.py
+++ client.py
@@ -1,21 +1,43 @@
 import socket
+import tkinter
+from tkinter import simpledialog
 
 HEADER = 64
 PORT = 5050
-SERVER = "10.110.41.216"
+SERVER = "169.254.170.125"
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+class Client:
+    def __init__ (self, host, port):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b" " * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
 
-send("Hello!")
+        self.root = tkinter.Tk()
+        
+
+        self.name = simpledialog.askstring("Nickname", "")
+        self.send(self.name)
+        self.send("!DISCONNECT")
+        
+
+        label = tkinter.Label(self.root, text=self.name)
+        label.pack()
+
+        self.root.mainloop()
+        
+
+    def send(self, msg):
+        message = msg.encode(FORMAT)    
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b" " * (HEADER - len(send_length))
+        self.conn.send(send_length)
+        #print(client.recv(1))
+        self.conn.send(message)
+
+
+
+client = Client(SERVER, PORT)
--- server.py
+++ server.py
@@ -12,11 +12,61 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            break
+"""
+def receive():
+    connected = True
+    while connected:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+
+        clients.send("NICK".encode(FORMAT))
+        nickname = client.recv(1024)
+        nicknames.append(nickname)
+
+        clients.append(client)
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
 def handle_conn(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected")
     
     connected = True
     while connected:
+        print(f"Connected with {str(addr)}")
+
         #   Get HEADER
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
@@ -25,16 +75,23 @@ def handle_conn(conn, addr):
 
             #   Recieve message with length
             msg = conn.recv(msg_length).decode(FORMAT)
-            
 
-            # Handle Disconnects
             if msg == DISCONNECT_MESSAGE:
-                connected = False
+                    connected = False
+                    break
+            nickname = msg
+            nicknames.append(nickname)
 
+            clients.append(conn)
 
-            print(f"[{addr}] {msg}")
+            print(nicknames)
+            #thread = threading.Thread(target=handle, args=(conn,))
+            #thread.start()
+
 
     conn.close()
+
+
 
 def start():
     server.listen()
@@ -43,7 +100,6 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_conn, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 print("[STARTING] server is starting...")
 start()
