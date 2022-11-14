03e5b738f74f5dda32e34c001bb2b4706a520297
Emilio Mendoza <Ironislife39@gmail.com>
2022-11-14 08:57:43 -0500

    Finished rework of mainserver.py with flask

--- mainserver.py
+++ mainserver.py
@@ -1,65 +1,39 @@
-import socket
-import threading
+from flask import Flask, request
 import pickle
 
-HEADER = 64
-PORT = 5000
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
+app = Flask(__name__)
+
+hosts = []
+
+@app.route("/")
+def home():
+  return "Please refer to <a href='https://github.com/Ironislife98/PythonChatApp' >Github</a> for api docs"
+
+@app.route('/api/gethosts')
+def gethosts():
+  print(hosts)
+  return str(pickle.dumps(hosts))
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-clients = []
-serverList = []
-
-def handle_conn(conn, addr):
-    ipaddr = None
-    connected = True
-    while connected:
-        # Get header
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-
-            #   Recieve message with length
-            msg = conn.recv(msg_length).decode(FORMAT)
-
-            if msg == DISCONNECT_MESSAGE:
-                    connected = False
-                    serverList.pop(serverList.index(ipaddr))
-                    clients.pop(clients.index(conn))
-                    break
-            
-            if msg == "closeconn":
-                connected = False
-                break
-
-            if ipaddr == None:
-                serverList.append(msg)
-                clients.append(conn)
-                ipaddr = msg
-                print(serverList)
-                print()
-                print(clients)
-            
-            if msg == "LIST":
-                conn.send(pickle.dumps(serverList))
-                print(f"Server list sent! : {serverList}")
-                print(f"Conn list: {clients}")
-            
+@app.route("/api/addserver/<ipaddr>")
+def addserver(ipaddr):
+  if ipaddr == request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr):
+    if ipaddr in hosts:
+      return str(pickle.dumps("Already added Host"))
+    else:
+      hosts.append(request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr))
+      return str(pickle.dumps("Added"))
+  else:
+    return str(pickle.dumps("CANNOT ADD OTHERS IP ADRESS"))
 
 
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_conn, args=(conn, addr))
-        thread.start()
+@app.route("/api/deleteserver/<ipaddr>")
+def delete(ipaddr):
+  if ipaddr == request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr):
+    hosts.pop(hosts.index(ipaddr))
+  else:
+    return str(pickle.dumps("Not your ip"))
+
 
 if __name__ == "__main__":
-    start()
+  app.run(host='0.0.0.0', port=5000)
